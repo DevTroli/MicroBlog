543032dec67c661e6110fdcfbae737cc19ab7804
Troli <brawlixo123@gmail.com>
2024-02-24 09:44:16 -0300

    feature: django admin

--- myblog/blog/admin.py
+++ myblog/blog/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models.post import Post
+
+# Alterar o modo de visualização dos Modelos no Django Admin
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from blog import views
+from blog.views import views
 
 urlpatterns= [
     path('', views.PostView.as_view(), name='home')
